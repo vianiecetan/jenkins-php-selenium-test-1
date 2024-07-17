508405e4e1f59b3b809698a0380dc94e02f13fa2
Carment14tham <carmenttham14@gmail.com>
2024-07-17 22:16:25 +0800

    second

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v D:\\Desktop\\Uni\\SIT\\Y2\\Year2Tri3\\SSD\\Lab\\Lab7b\\Lab7b\\jenkins-php-selenium-test\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v D:\\Desktop\\Uni\\SIT\\Y2\\Year2Tri3\\SSD\\Lab\\jenkins-php-selenium-test\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
