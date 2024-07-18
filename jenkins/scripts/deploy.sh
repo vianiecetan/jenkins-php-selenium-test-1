da198871fd1b5f6a794b7bf948e5e0d6c5a82117
vianiecetan <vianiece04@gmail.com>
2024-07-18 21:50:12 +0800

    update filepath

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v //c:/Users/Vianiece/Desktop/SSD/lab7b/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\Vianiece\\Desktop\\SSD\\lab7b\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 set +x
 
 echo 'Now...'
