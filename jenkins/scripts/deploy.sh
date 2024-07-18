b67235d0f7adea05e846c7b5b5288b0c6e4aa674
vianiecetan <vianiece04@gmail.com>
2024-07-18 21:15:29 +0800

    clone

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Uni\\"Year 2"\\Y2T3\\SSD\\Lab\\Lab7b\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\Vianiece\\Desktop\\SSD\\lab7b\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
